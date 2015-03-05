f9205660fdd309650ca95c7b0c37b8e9e079fbb5
Girou Maxime <maxime.girou@laposte.net>
2015-03-05 15:34:50 +0100

    Copie des fichiers

---
+++ js/popup.js
@@ -0,0 +1,33 @@
+
+//fonction pour afficher des popUp et les supprimer ensuite
+	var idPopup=0;
+	function setPopup(text,type, time)
+	{
+		//verifications
+		if( typeof(time) == 'undefined' ){time= 4000;}
+		if(type!="success" && type!="danger" && type!="info" && type!="warning" && type!="default"){type="default";}
+		if(text=="" || text== 'undefined'){console.log('Popup.js : No text = No display');return false;}
+		//
+
+		var popup=[];
+
+		popup="<p id='popup_"+idPopup+"' class='popup popup-"+type+"'>"+text+"</p>";
+		
+		$('#popup-div').append(popup);
+		hidePopup(idPopup, time);
+		idPopup++;
+
+		return true;
+	}
+	
+	function hidePopup(id, time)
+	{
+		setTimeout(function()
+		{
+			$('#popup_'+id).animate({opacity: 0,height: "toggle"}, 500);
+		}, time);
+		setTimeout(function(){$('#popup_'+id).remove();}, time+1000);
+
+		return true;
+	}
+
